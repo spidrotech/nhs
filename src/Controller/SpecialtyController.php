3969628d36abd04e14bf3fdb8f07f582cbb8c122
amri debian <amri@debian>
2019-09-18 08:42:03 +0200

    update composer

--- src/Controller/SpecialtyController.php
+++ src/Controller/SpecialtyController.php
@@ -14,57 +14,54 @@ use App\Form\SpecialtyType;
  * Specialty controller.
  * @Route("/api", name="api_")
  */
-class SpecialtyController extends FOSRestController
-{
+class SpecialtyController extends FOSRestController {
 
-      /**
-   * Lists all specialties.
-   * @Rest\Get("/specialties", name="get collection of specialties")
-   *
-   * @return Response
-   */
-    public function getSpecialtiesAction()
-    {
-    	$repository = $this->getDoctrine()->getRepository(Specialty::class);
-	    $specialties = $repository->findall();
-	    return $this->handleView($this->view($specialties));
-    }
-
-      /**
-   * Get a specialty.
-   * @Rest\Get("/specialty/{id}", name="get resource specialties")
-   *
-   * @return Response
-   */
-    public function getSpecialtyAction(Request $request)
-    {
-    	$repository = $this->getDoctrine()->getRepository(Specialty::class);
-	    $specialty = $repository->find($request->get('id'));
-	    if (empty($specialty)) {
-      		return $this->handleView($this->view(['status' => 'ko'], Response::HTTP_NOT_FOUND));
-        }
-	    return $this->handleView($this->view($specialty));
-    }
     /**
-   * Create Specialty.
-   * @Rest\Post("/specialty", name="post specialty")
-   *
-   * @return Response
-   */
-  public function postSpecialtyAction(Request $request)
-  {
-    $specialty = new Specialty();
-    $form = $this->createForm(SpecialtyType::class, $specialty);
-
-    $data = json_decode($request->getContent(), true);
-    $form->submit($data);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($specialty);
-      $em->flush();
-      return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+     * Lists all specialties.
+     * @Rest\Get("/specialties", name="get collection of specialties")
+     *
+     * @return Response
+     */
+    public function getSpecialtiesAction() {
+        $repository = $this->getDoctrine()->getRepository(Specialty::class);
+        $specialties = $repository->findall();
+        return $this->handleView($this->view($specialties));
     }
-    return $this->handleView($this->view($form->getErrors()));
-  }
+
+    /**
+     * Get a specialty.
+     * @Rest\Get("/specialty/{id}", name="get resource specialties")
+     *
+     * @return Response
+     */
+    public function getSpecialtyAction(Request $request) {
+        $repository = $this->getDoctrine()->getRepository(Specialty::class);
+        $specialty = $repository->find($request->get('id'));
+        if (empty($specialty)) {
+            return $this->handleView($this->view(['status' => 'ko'], Response::HTTP_NOT_FOUND));
+        }
+        return $this->handleView($this->view($specialty));
+    }
+
+    /**
+     * Create Specialty.
+     * @Rest\Post("/specialty", name="post specialty")
+     *
+     * @return Response
+     */
+    public function postSpecialtyAction(Request $request) {
+        $specialty = new Specialty();
+        $form = $this->createForm(SpecialtyType::class, $specialty);
+
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($specialty);
+            $em->flush();
+            return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+        }
+        return $this->handleView($this->view($form->getErrors()));
+    }
 }
